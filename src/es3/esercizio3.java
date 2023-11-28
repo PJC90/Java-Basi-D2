8636350834c2f3eae7d4d32d5f25d129b7938484
Pierpaolo Colasante <colasantepierpaolo@gmail.com>
2023-11-28 16:45:32 +0100

    es 3

--- src/es3/esercizio3.java
+++ src/es3/esercizio3.java
@@ -1,6 +1,5 @@
 package es3;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class esercizio3 {
@@ -14,11 +13,11 @@ public class esercizio3 {
                 System.out.println("fine, hai messo la q");
                 break;
             }
-            String[] caratteri = input.split(",");
+
             System.out.println("caratteri separati:");
             int i = 0;
-            while (i < caratteri.length) {
-                System.out.println(caratteri[i].trim());
+            while (i < input.length()) {
+                System.out.println(input.charAt(i));
                 i++;
             }
         }
